fe154c19e24c4fedc643af4059cc75d7173a32b4
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-03-06 10:53:53 -0300

    local storage e api JSON

--- Aplicacoes-Web-Ageis/fokus-local-storage/script-crud.js
+++ Aplicacoes-Web-Ageis/fokus-local-storage/script-crud.js
@@ -1,9 +1,19 @@
-//encontrando o botao add tarefa
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
-//capturando o formulário de adicionar tarefa
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 // quando o botao for clicado, sera feita a alternancia da classe hidden
 btnAdicionarTarefa.addEventListener('click', () => {
   formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+  const tarefa = {
+    descricao: textArea.value
+  }
+  tarefas.push(tarefa)
+  localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
